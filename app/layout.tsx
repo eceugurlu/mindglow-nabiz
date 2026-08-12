71e43ecbac6ddd26aa1bdee010d32c9e3c9f764c
eceugurlu <ece.ugurlu@boun.edu.tr>
2026-08-12 04:26:19 +0300

    iphone viewport ve bug fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata,Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -31,3 +31,9 @@ export default function RootLayout({
     </html>
   );
 }
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
